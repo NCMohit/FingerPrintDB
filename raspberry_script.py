e0f4ce83dbaf21b5bac452dd50e51569c094025a
N C Mohit <44508697+NCMohit@users.noreply.github.com>
2019-06-08 22:21:43 +0530

    Update raspberry_script.py

--- raspberry_script.py
+++ raspberry_script.py
@@ -1,8 +1,9 @@
 # Add this to the raspberry pi script to send fingerprint info to server
 import requests
- 
-data_from_pi = 'Changeme (Fingerprint data)'
-userid = 1
-response = requests.post('http://127.0.0.1:5000/pi', json=(data_from_pi,userid)) #Change server IP
+#Finger for buyer identification, userpass for seller identification
+finger = 'Changeme (Fingerprint data)'
+username = 'cyka'
+password = 'blyat1'
+response = requests.post('http://127.0.0.1:5000/pitransactionrequest', json=(username,password,finger)) #Change server IP
 if response.ok:
     print(response.json())
