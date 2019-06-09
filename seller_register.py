dd4f10a4522dda76b7c27c1c5146fb4f32818921
N C Mohit <44508697+NCMohit@users.noreply.github.com>
2019-06-09 21:35:00 +0530

    Update seller_register.py

--- seller_register.py
+++ seller_register.py
@@ -1,7 +1,8 @@
 # This script needs to be run in the web server to register the user in database server
 import requests
-username = 'testuser'
+username = 'testseller'
 password = 'testpass'
-response = requests.post('http://127.0.0.1:5000/piregister', json=(username,password)) #Change server IP
+bank_details = 'acc-no'
+response = requests.post('http://127.0.0.1:5000/piregisterseller', json=(username,password,bank_details)) #Change server IP
 if response.ok:
     print(response.json())
